05714f493720268ffe00c4103b2804e75da0f512
Mehdi Bari <mbari@e2r4p1.1337.ma>
2021-02-27 16:54:25 +0100

    grafana's docker file is done just need dashboard config

--- phpMyAdmin/src/php-setup.sh
+++ phpMyAdmin/src/php-setup.sh
@@ -5,3 +5,4 @@ wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-l
 tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
+mv /tmp/config.inc.php /var/www/localhost/htdocs/config.inc.php
